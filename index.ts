700f9c2859e22a2d9348e80ec877f535c7df0316
Skwal98 <levkalex98@gmail.com>
2021-08-07 01:32:09 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
 console.log('test');
+
+interface Lengthwise {
+  length: number;
+}
+
+interface Sizehwise {
+  size: number;
+}
+
+function logging<T extends Lengthwise & Sizehwise>(a: T): T {
+  return a;
+}
+
+///////////////////////
+
+interface Car {
+  model: number;
+  year: number;
+}
+
+type CarKeyOff = keyof Car;
+
+type indexerType = { [x: number]: Car };
+
+function test(par: indexerType) {
+  console.log(par['audi']);
+}
+
+var par: indexerType;
+par = {};
+var audi = { model: 21, year: 2010 } as Car;
+par['audi'] = audi;
+test(par);
+
+console.log();
+
+/////////////////////////////////////
+
